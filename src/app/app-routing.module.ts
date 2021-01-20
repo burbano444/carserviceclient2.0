1d3707a44ecf5a6c510de86734d0251fa30e2692
burbano444 <burbano444@hotmail.com>
2021-01-19 19:25:52 -0500

    agregando proyecto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CarListComponent } from './car-list/car-list.component';
+import { CarEditComponent } from './car-edit/car-edit.component';
+import { OwnerListComponent } from './owner-list/owner-list.component';
+import { OwnerEditComponent } from './owner-edit/owner-edit.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/car-list', pathMatch: 'full' },
+  {
+    path: 'car-list',
+    component: CarListComponent
+  },
+  {
+    path: 'car-add',
+    component: CarEditComponent
+  },
+  {
+    path: 'car-edit/:id',
+    component: CarEditComponent
+  },
+  {
+    path: 'owner-list',
+    component: OwnerListComponent
+  },
+  {
+    path: 'owner-edit/:name',
+    component: OwnerEditComponent
+  },
+  {
+    path: 'owner-add',
+    component: OwnerEditComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
